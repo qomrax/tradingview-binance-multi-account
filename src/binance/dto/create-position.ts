1a5c864d9ead1ee3513281937b3b0b03bf7f1faa
fatih <qraxiss@gmail.com>
2025-03-14 19:10:34 +0300

    transform tradingview tickers to binance tickers.

--- src/binance/dto/create-position.ts
+++ src/binance/dto/create-position.ts
@@ -1,11 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsIn, IsString } from 'class-validator';
 import { OrderSide_LT } from 'binance-api-node';
+import { Transform } from 'class-transformer';
 
 export class OpenPositionDto {
     @ApiProperty({
-        example: "XRPUSDT",
+        example: "XRPUSDT.P",
         description: 'Coin pair.',
+    })
+    @Transform(({ value }: { value: string }) => {
+        const upperCased = value.toUpperCase()
+        const replaced = upperCased.replace(".P", "")
+        return replaced
     })
     @IsString()
     symbol: string;
