b36c0bb80e115fda640dbba23f1b81346615c88c
fatih <qraxiss@gmail.com>
2025-04-17 04:42:03 +0300

    Update create-db.sh

--- scripts/create-db.sh
+++ scripts/create-db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting database setup..."
-mysql -u root -p < create-db.sql
+sudo mysql -u root -p < create-db.sql
 if [ $? -eq 0 ]; then
   echo "Database setup completed successfully."
   echo "- Database 'api-ramazan' created"
