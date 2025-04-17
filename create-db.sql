f37c285684d1a7c24ff8f093ab4a4935731d4efd
fatih <qraxiss@gmail.com>
2025-04-17 04:37:35 +0300

    Update create-db.sql

--- create-db.sql
+++ create-db.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS `api-hakan`;
 
 -- Create the admin user if it doesn't exist
 CREATE USER IF NOT EXISTS 'admin'@'%' IDENTIFIED BY 'admin123';
-CREATE USER IF NOT EXISTS 'admin'@'localhost' IDENTIFIED BY 'admin123';
+CREATE USER IF NOT EXISTS 'admin'@'127.0.0.1' IDENTIFIED BY 'admin123';
 
 -- Grant all privileges to the admin user on the database
 GRANT ALL PRIVILEGES ON `api-hakan`.* TO 'admin'@'%';
