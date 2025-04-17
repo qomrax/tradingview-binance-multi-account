449fc9874c86f2b4be8a1ad02a11d9b372d0d144
fatih <qraxiss@gmail.com>
2025-04-17 04:28:14 +0300

    Revert "personal"
    This reverts commit ad1261dfc4a0881811fdab55c5cfe5d0cb8e0566.

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   apps: [{
-    name: "api-hakan",
+    name: "tradingview-binance-nestjs",
     script: "yarn start"
   }]
 }
--- scripts/nginx.sh
+++
@@ -1,5 +0,0 @@
-sudo cp conf.nginx /etc/nginx/sites-available/api.hakan.qraxiss.com
-
-sudo ln -s /etc/nginx/sites-available/api.hakan.qraxiss.com /etc/nginx/sites-enabled/
-sudo nginx -t
-sudo systemctl restart nginx
